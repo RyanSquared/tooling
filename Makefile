4e391fbca02885857166af486e0dd8b849b584c7
RyanSquared <ryan@hashbang.sh>
2022-05-01 00:30:12 -0400

    make/tools: limit concurrent go build processes to 8
    Reasoning: some of the projects are large enough that they cause the
    system to run out of memory before they finish building. when limiting
    concurrent build processes to 8, the project still builds in a
    reasonably fast amount of time, but doesn't use up a significant amount
    of memory, at least on my Ryzen 5900 and 16GB RAM system. i think given
    the CPU speed is one of the fastest available right now for desktop
    machines means that it's definitely going to be limited by CPU rather
    than memory.
    
    time to download more RAM

--- Makefile
+++ Makefile
@@ -1,3 +1,13 @@
-include $(PWD)/config/tools.env
-include $(PWD)/make/tools.mk
+.PHONY: default
+default: tools
 
+include $(PWD)/make/tools.mk
+include $(PWD)/config/tools.env
+
+.PHONY: clean
+clean: clean-tools clean-build
+
+.PHONY: clean-build
+clean-build:
+	# yes, -f is necessary, because of git meta files
+	rm -rf build
--- make/tools.mk
+++ make/tools.mk
@@ -10,43 +10,47 @@ define build-go
 	sh -c "cd build/$(1) && $(4)"
 endef
 
+# 8 processes is probably enough to build all processes on a machine with 8 GB
+# of RAM without running out of RAM
+GOFLAGS=-p 8 -v -trimpath -ldflags=-w
+
+tools: tools/kubectl tools/k9s tools/sops tools/viaduct.ai/v1/ksops/ksops tools/kustomize tools/terraform tools/terraform-ignition tools/terraform-aws
+
 .PHONY: clean-tools
 clean-tools:
 	rm -rf tools/*
 
-tools: tools/kubectl tools/k9s tools/sops tools/viaduct.ai/v1/ksops/ksops tools/kustomize tools/terraform tools/terraform-ignition tools/terraform-aws
-
 tools/kubectl:
-	$(eval CMD="go build -v -trimpath -ldflags=-w -o $(PWD)/tools/kubectl $(KUBECTL_PKG)")
+	$(eval CMD="go build $(GOFLAGS) -o $(PWD)/tools/kubectl $(KUBECTL_PKG)")
 	$(call build-go,kubectl,"$(KUBECTL_URL)","$(KUBECTL_REF)","$(CMD)")
 
 tools/k9s:
-	$(eval CMD="go build -v -trimpath -ldflags=-w -o $(PWD)/tools/k9s")
+	$(eval CMD="go build $(GOFLAGS) -o $(PWD)/tools/k9s")
 	$(call build-go,k9s,"$(K9S_URL)","$(K9S_REF)","$(CMD)")
 
 tools/sops:
-	$(eval CMD="go build -v -trimpath -ldflags=-w -o $(PWD)/tools/sops go.mozilla.org/sops/v3/cmd/sops")
+	$(eval CMD="go build $(GOFLAGS) -o $(PWD)/tools/sops go.mozilla.org/sops/v3/cmd/sops")
 	$(call build-go,sops,"$(SOPS_URL)","$(SOPS_REF)","$(CMD)")
 
 tools/viaduct.ai/v1/ksops/ksops:
 	# Note: This file structure is important, as it must be mirrored where
 	# kustomize looks for plugins.
 	# See also: https://github.com/kubernetes-sigs/kustomize/blob/master/api/konfig/plugins.go#L19-L21
-	$(eval CMD="go build -v -trimpath -ldflags=-w -o $(PWD)/tools/viaduct.ai/v1/ksops/ksops")
+	$(eval CMD="go build $(GOFLAGS) -o $(PWD)/tools/viaduct.ai/v1/ksops/ksops")
 	$(call build-go,ksops,"$(KSOPS_URL)","$(KSOPS_REF)","$(CMD)")
 
 tools/kustomize:
-	$(eval CMD="cd kustomize && go build -v -trimpath -ldflags=-w -o $(PWD)/tools/kustomize")
+	$(eval CMD="cd kustomize && go build $(GOFLAGS) -o $(PWD)/tools/kustomize")
 	$(call build-go,kustomize,"$(KUSTOMIZE_URL)","$(KUSTOMIZE_REF)","$(CMD)")
 
 tools/terraform:
-	$(eval CMD="go build -v -trimpath -ldflags=-w -o $(PWD)/tools/terraform")
+	$(eval CMD="go build $(GOFLAGS) -o $(PWD)/tools/terraform")
 	$(call build-go,terraform,"$(TERRAFORM_URL)","$(TERRAFORM_REF)","$(CMD)")
 
 tools/terraform-ignition:
-	$(eval CMD="go build -v -trimpath -ldflags=-w -o $(PWD)/tools/terraform-ignition")
+	$(eval CMD="go build $(GOFLAGS) -o $(PWD)/tools/terraform-ignition")
 	$(call build-go,terraform-ignition,"$(TERRAFORM_IGNITION_URL)","$(TERRAFORM_IGNITION_REF)","$(CMD)")
 
 tools/terraform-aws:
-	$(eval CMD="go build -v -trimpath -ldflags=-w -o $(PWD)/tools/terraform-aws")
+	$(eval CMD="go build $(GOFLAGS) -o $(PWD)/tools/terraform-aws")
 	$(call build-go,terraform-aws,"$(TERRAFORM_AWS_URL)","$(TERRAFORM_AWS_REF)","$(CMD)")
