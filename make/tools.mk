d9892bef49bb4c001b084698aa7c04d6814df3a5
RyanSquared <ryan@hashbang.sh>
2022-06-23 01:46:58 -0400

    make/tools: break out downloading code and building code

--- make/tools.mk
+++ make/tools.mk
@@ -1,76 +1,111 @@
-# TODO: can we optimize out command? probably not because of _PKG values
-# Command most times will be in the format of:
-# go build -v -trimpath -ldflags=-w -o $(PWD)/tools/k9s
-# that might be optional
-# Arguments: 1: binary, 2: url, 3: ref/tag, 4: command
-define build-go
-	mkdir -p build tools
-	git clone $(2) build/$(1) || true
-	git -C build/$(1) checkout $(3)
-	sh -c "cd build/$(1) && $(4)"
-endef
-
 # 8 processes is probably enough to build all processes on a machine with 8 GB
 # of RAM without running out of RAM
 GOFLAGS=-p 8 -v -trimpath -ldflags=-w
 
-tools: tools/kubectl tools/k9s tools/sops tools/viaduct.ai/v1/ksops/ksops tools/kustomize tools/terraform tools/terraform-ignition tools/terraform-aws
+# Arguments: 1: binary, 2: pkg?, 3: output?, 4: build directory?
+define build-go
+	sh -c "cd build/$(1)$(if $(4),/$(4),) && go build $(GOFLAGS) -o $(PWD)/tools/$(if $(3),$(3),$(1))$(if $(2), $(2),)"
+endef
+
+# Arguments: 1: binary, 2: repository, 3: ref
+define clone-repo
+	mkdir -p build
+	git clone $(2) build/$(1)
+	git -C build/$(1) checkout $(3)
+	sh -c "test `git -C build/$(1) rev-parse HEAD` = $(3)"
+endef
+
+build: build/kubectl build/k9s build/sops build/ksops
+build: build/kustomize build/terraform build/terraform-ignition
+build: build/terraform-aws build/terraform-digitalocean build/terraform-local
+build: build/terraform-null build/talosctl build/yq
+
+tools: tools/kubectl tools/k9s tools/sops tools/viaduct.ai/v1/ksops/ksops
+tools: tools/kustomize tools/terraform tools/terraform-ignition
+tools: tools/terraform-aws tools/terraform-digitalocean tools/terraform-local
+tools: tools/terraform-null tools/talosctl tools/yq
 
 .PHONY: clean-tools
 clean-tools:
 	rm -rf tools/*
 
-tools/kubectl:
-	$(eval CMD="go build $(GOFLAGS) -o $(PWD)/tools/kubectl $(KUBECTL_PKG)")
-	$(call build-go,kubectl,"$(KUBECTL_URL)","$(KUBECTL_REF)","$(CMD)")
+build/kubectl:
+	$(call clone-repo,kubectl,$(KUBECTL_URL),$(KUBECTL_REF))
 
-tools/k9s:
-	$(eval CMD="go build $(GOFLAGS) -o $(PWD)/tools/k9s")
-	$(call build-go,k9s,"$(K9S_URL)","$(K9S_REF)","$(CMD)")
+tools/kubectl: build/kubectl
+	$(call build-go,kubectl,$(KUBECTL_PKG))
 
-tools/sops:
-	$(eval CMD="go build $(GOFLAGS) -o $(PWD)/tools/sops go.mozilla.org/sops/v3/cmd/sops")
-	$(call build-go,sops,"$(SOPS_URL)","$(SOPS_REF)","$(CMD)")
+build/k9s:
+	$(call clone-repo,k9s,$(K9S_URL),$(K9S_REF))
 
-tools/viaduct.ai/v1/ksops/ksops:
+tools/k9s: build/k9s
+	$(call build-go,k9s)
+
+build/sops:
+	$(call clone-repo,sops,$(SOPS_URL),$(SOPS_REF))
+
+tools/sops: build/sops
+	$(call build-go,sops,go.mozilla.org/sops/v3/cmd/sops)
+
+build/ksops:
+	$(call clone-repo,ksops,$(KSOPS_URL),$(KSOPS_REF))
+
+tools/viaduct.ai/v1/ksops/ksops: build/ksops
 	# Note: This file structure is important, as it must be mirrored where
 	# kustomize looks for plugins.
 	# See also: https://github.com/kubernetes-sigs/kustomize/blob/master/api/konfig/plugins.go#L19-L21
-	$(eval CMD="go build $(GOFLAGS) -o $(PWD)/tools/viaduct.ai/v1/ksops/ksops")
-	$(call build-go,ksops,"$(KSOPS_URL)","$(KSOPS_REF)","$(CMD)")
+	$(call build-go,ksops,,viaduct.ai/v1/ksops/ksops)
 
-tools/kustomize:
-	$(eval CMD="cd kustomize && go build $(GOFLAGS) -o $(PWD)/tools/kustomize")
-	$(call build-go,kustomize,"$(KUSTOMIZE_URL)","$(KUSTOMIZE_REF)","$(CMD)")
+build/kustomize:
+	$(call clone-repo,kustomize,$(KUSTOMIZE_URL),$(KUSTOMIZE_REF))
 
-tools/terraform:
-	$(eval CMD="go build $(GOFLAGS) -o $(PWD)/tools/terraform")
-	$(call build-go,terraform,"$(TERRAFORM_URL)","$(TERRAFORM_REF)","$(CMD)")
+tools/kustomize: build/kustomize
+	$(call build-go,kustomize,,,kustomize)
 
-tools/terraform-ignition:
-	$(eval CMD="go build $(GOFLAGS) -o $(PWD)/tools/terraform-ignition")
-	$(call build-go,terraform-ignition,"$(TERRAFORM_IGNITION_URL)","$(TERRAFORM_IGNITION_REF)","$(CMD)")
+build/terraform:
+	$(call clone-repo,terraform,$(TERRAFORM_URL),$(TERRAFORM_REF))
 
-tools/terraform-aws:
-	$(eval CMD="go build $(GOFLAGS) -o $(PWD)/tools/terraform-aws")
-	$(call build-go,terraform-aws,"$(TERRAFORM_AWS_URL)","$(TERRAFORM_AWS_REF)","$(CMD)")
+tools/terraform: build/terraform
+	$(call build-go,terraform)
 
-tools/terraform-digitalocean:
-	$(eval CMD="go build $(GOFLAGS) -o $(PWD)/tools/terraform-digitalocean")
-	$(call build-go,terraform-digitalocean,"$(TERRAFORM_DIGITALOCEAN_URL)","$(TERRAFORM_DIGITALOCEAN_REF)","$(CMD)")
+build/terraform-ignition:
+	$(call clone-repo,terraform-ignition,$(TERRAFORM_IGNITION_URL),$(TERRAFORM_IGNITION_REF))
 
-tools/terraform-local:
-	$(eval CMD="go build $(GOFLAGS) -o $(PWD)/tools/terraform-local")
-	$(call build-go,terraform-local,"$(TERRAFORM_LOCAL_URL)","$(TERRAFORM_LOCAL_REF)","$(CMD)")
+tools/terraform-ignition: build/terraform-ignition
+	$(call build-go,terraform-ignition)
 
-tools/terraform-null:
-	$(eval CMD="go build $(GOFLAGS) -o $(PWD)/tools/terraform-null")
-	$(call build-go,terraform-null,"$(TERRAFORM_NULL_URL)","$(TERRAFORM_NULL_REF)","$(CMD)")
+build/terraform-aws:
+	$(call clone-repo,terraform-aws,$(TERRAFORM_AWS_URL),$(TERRAFORM_AWS_REF))
 
-tools/talosctl:
-	$(eval CMD="go build $(GOFLAGS) -o $(PWD)/tools/talosctl $(TALOSCTL_PKG)")
-	$(call build-go,talosctl,"$(TALOSCTL_URL)","$(TALOSCTL_REF)","$(CMD)")
+tools/terraform-aws: build/terraform-aws
+	$(call build-go,terraform-aws)
 
-tools/yq:
-	$(eval CMD="go build $(GOFLAGS) -o $(PWD)/tools/yq")
-	$(call build-go,yq,"$(YQ_URL)","$(YQ_REF)","$(CMD)")
+build/terraform-digitalocean:
+	$(call clone-repo,terraform-digitalocean,$(TERRAFORM_DIGITALOCEAN_URL),$(TERRAFORM_DIGITALOCEAN_REF))
+
+tools/terraform-digitalocean: build/terraform-digitalocean
+	$(call build-go,terraform-digitalocean)
+
+build/terraform-local:
+	$(call clone-repo,terraform-local,$(TERRAFORM_LOCAL_URL),$(TERRAFORM_LOCAL_REF))
+
+tools/terraform-local: build/terraform-local
+	$(call build-go,terraform-local)
+
+build/terraform-null:
+	$(call clone-repo,terraform-null,$(TERRAFORM_NULL_URL),$(TERRAFORM_NULL_REF))
+
+tools/terraform-null: build/terraform-null
+	$(call build-go,terraform-null)
+
+build/talosctl:
+	$(call clone-repo,talosctl,$(TALOSCTL_URL),$(TALOSCTL_REF))
+
+tools/talosctl: build/talosctl
+	$(call build-go,talosctl,$(TALOSCTL_PKG))
+
+build/yq:
+	$(call clone-repo,yq,$(YQ_URL),$(YQ_REF))
+
+tools/yq: build/yq
+	$(call build-go,yq)
