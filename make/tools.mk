da7a14630e773134d8b33248e397cf42cb7dbbe9
RyanSquared <ryan@hashbang.sh>
2022-08-05 14:36:20 -0400

    make/tools: add config/tools.env dependency and add `git pull`

--- make/tools.mk
+++ make/tools.mk
@@ -10,7 +10,7 @@ endef
 # Arguments: 1: binary, 2: repository, 3: ref
 define clone-repo
 	mkdir -p build
-	git clone $(2) build/$(1)
+	sh -c "git clone $(2) build/$(1) || test -d build/$(1) && git -C build/$(1) fetch"
 	git -C build/$(1) checkout $(3)
 	sh -c "test `git -C build/$(1) rev-parse HEAD` = $(3)"
 endef
@@ -29,25 +29,25 @@ tools: tools/terraform-null tools/talosctl tools/yq
 clean-tools:
 	rm -rf tools/*
 
-build/kubectl:
+build/kubectl: config/tools.env
 	$(call clone-repo,kubectl,$(KUBECTL_URL),$(KUBECTL_REF))
 
 tools/kubectl: build/kubectl
 	$(call build-go,kubectl,$(KUBECTL_PKG))
 
-build/k9s:
+build/k9s: config/tools.env
 	$(call clone-repo,k9s,$(K9S_URL),$(K9S_REF))
 
 tools/k9s: build/k9s
 	$(call build-go,k9s)
 
-build/sops:
+build/sops: config/tools.env
 	$(call clone-repo,sops,$(SOPS_URL),$(SOPS_REF))
 
 tools/sops: build/sops
 	$(call build-go,sops,go.mozilla.org/sops/v3/cmd/sops)
 
-build/ksops:
+build/ksops: config/tools.env
 	$(call clone-repo,ksops,$(KSOPS_URL),$(KSOPS_REF))
 
 tools/viaduct.ai/v1/ksops/ksops: build/ksops
@@ -56,55 +56,55 @@ tools/viaduct.ai/v1/ksops/ksops: build/ksops
 	# See also: https://github.com/kubernetes-sigs/kustomize/blob/master/api/konfig/plugins.go#L19-L21
 	$(call build-go,ksops,,viaduct.ai/v1/ksops/ksops)
 
-build/kustomize:
+build/kustomize: config/tools.env
 	$(call clone-repo,kustomize,$(KUSTOMIZE_URL),$(KUSTOMIZE_REF))
 
 tools/kustomize: build/kustomize
 	$(call build-go,kustomize,,,kustomize)
 
-build/terraform:
+build/terraform: config/tools.env
 	$(call clone-repo,terraform,$(TERRAFORM_URL),$(TERRAFORM_REF))
 
 tools/terraform: build/terraform
 	$(call build-go,terraform)
 
-build/terraform-ignition:
+build/terraform-ignition: config/tools.env
 	$(call clone-repo,terraform-ignition,$(TERRAFORM_IGNITION_URL),$(TERRAFORM_IGNITION_REF))
 
 tools/terraform-ignition: build/terraform-ignition
 	$(call build-go,terraform-ignition)
 
-build/terraform-aws:
+build/terraform-aws: config/tools.env
 	$(call clone-repo,terraform-aws,$(TERRAFORM_AWS_URL),$(TERRAFORM_AWS_REF))
 
 tools/terraform-aws: build/terraform-aws
 	$(call build-go,terraform-aws)
 
-build/terraform-digitalocean:
+build/terraform-digitalocean: config/tools.env
 	$(call clone-repo,terraform-digitalocean,$(TERRAFORM_DIGITALOCEAN_URL),$(TERRAFORM_DIGITALOCEAN_REF))
 
 tools/terraform-digitalocean: build/terraform-digitalocean
 	$(call build-go,terraform-digitalocean)
 
-build/terraform-local:
+build/terraform-local: config/tools.env
 	$(call clone-repo,terraform-local,$(TERRAFORM_LOCAL_URL),$(TERRAFORM_LOCAL_REF))
 
 tools/terraform-local: build/terraform-local
 	$(call build-go,terraform-local)
 
-build/terraform-null:
+build/terraform-null: config/tools.env
 	$(call clone-repo,terraform-null,$(TERRAFORM_NULL_URL),$(TERRAFORM_NULL_REF))
 
 tools/terraform-null: build/terraform-null
 	$(call build-go,terraform-null)
 
-build/talosctl:
+build/talosctl: config/tools.env
 	$(call clone-repo,talosctl,$(TALOSCTL_URL),$(TALOSCTL_REF))
 
 tools/talosctl: build/talosctl
 	$(call build-go,talosctl,$(TALOSCTL_PKG))
 
-build/yq:
+build/yq: config/tools.env
 	$(call clone-repo,yq,$(YQ_URL),$(YQ_REF))
 
 tools/yq: build/yq
